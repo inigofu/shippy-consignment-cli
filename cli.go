a0831eb88f768708c3fa61c2deaa33b0f46e8fd1
Iñigo Fuente <fuente.inigo@gmail.com>
2018-04-23 12:28:18 +0200

    inicio

--- cli.go
+++ cli.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"os"
 
-	pb "github.com/EwanValentine/shippy-consignment-service/proto/consignment"
+	pb "github.com/inigofu/shippy-consignment-service/proto/consignment"
 	microclient "github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/cmd"
 	"github.com/micro/go-micro/metadata"
@@ -33,7 +33,7 @@ func main() {
 	cmd.Init()
 
 	// Create new greeter client
-	client := pb.NewShippingServiceClient("go.micro.srv.consignment", microclient.DefaultClient)
+	client := pb.NewConsignmentServiceClient("go.micro.srv.consignment", microclient.DefaultClient)
 
 	// Contact the server and print out its response.
 	file := defaultFilename
@@ -57,13 +57,13 @@ func main() {
 		"token": token,
 	})
 
-	r, err := client.CreateConsignment(ctx, consignment)
+	r, err := client.Create(ctx, consignment)
 	if err != nil {
 		log.Fatalf("Could not create: %v", err)
 	}
 	log.Printf("Created: %t", r.Created)
 
-	getAll, err := client.GetConsignments(ctx, &pb.GetRequest{})
+	getAll, err := client.Get(ctx, &pb.GetRequest{})
 	if err != nil {
 		log.Fatalf("Could not list consignments: %v", err)
 	}
